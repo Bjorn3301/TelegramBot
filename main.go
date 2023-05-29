3f2f50ca22be143a0a0c39a0aaf4a11eed61e345
Bjorn <ilyavakulich3301@gmail.com>
2023-05-29 22:40:25 +0300

    add reply message in bot

--- main.go
+++ main.go
@@ -1,63 +1,15 @@
 package main
 
 import (
-	"bytes"
-	"encoding/json"
 	"fmt"
-	"io/ioutil"
-	"log"
-	"net/http"
-	"strconv"
+	"os"
 )
 
-// Точка входа
 func main() {
-	botToken := "6211018847:AAEf6Gv_9_1evd2z-4nBlPsbkx2WTnZGQRQ"
-	botApi := "https://api.telegram.org/bot"
-	//https://api.telegram.org/bot<token>/METHOD_NAME
-	botUrl := botApi + botToken
-	offset := 0
-	for {
-		updates, err := getUpdates(botUrl, offset)
-		if err != nil {
-			log.Println("Some went wrong: ", err.Error())
-		}
-		for _, update := range updates {
-			err = respond(botUrl, update)
-			offset = update.UpdateId + 1
-		}
-		fmt.Println(updates)
+	token := os.Getenv("TELEGRAM_APITOKEN")
+	if token == "" {
+		fmt.Println("Переменная окружения TELEGRAM_APITOKEN не установлена")
+	} else {
+		fmt.Println("Переменная окружения TELEGRAM_APITOKEN установлена:", token)
 	}
-}
-
-func getUpdates(botUrl string, offset int) ([]Update, error) {
-	resp, err := http.Get(botUrl + "/getUpdates" + "?offest = " + strconv.Itoa(offset))
-	if err != nil {
-		return nil, err
-	}
-	defer resp.Body.Close()
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return nil, err
-	}
-	var restResponse Restresponse
-	err = json.Unmarshal(body, &restResponse)
-	if err != nil {
-		return nil, err
-	}
-	return restResponse.Result, nil
-}
-func respond(botUrl string, update Update) error {
-	var BotMessage BotMessage
-	BotMessage.ChatId = update.Message.Chat.ChatId
-	BotMessage.Text = update.Message.Text
-	buf, err := json.Marshal(BotMessage)
-	if err != nil {
-		return err
-	}
-	_, err = http.Post(botUrl+"/sendMessage", "application/json", bytes.NewBuffer(buf))
-	if err != nil {
-		return err
-	}
-	return nil
 }
